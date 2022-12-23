68d497094039525928a1a4bea45eba9ca6f7b9fe
kompaniycev <92985369+kompaniycev@users.noreply.github.com>
2022-12-23 13:38:24 +0300

    first commit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+using teamcity1;
+
+
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Test]
+        // [Ignore("This test is disabled")]
+        public static void TestOne()
+        {
+            Assert.AreEqual(Program.Factorial(1), 1);
+        }
+
+        [Test]
+        // [Ignore("This test is disabled")]
+        public static void TestTwo()
+        {
+            Assert.AreEqual(Program.Factorial(6), 720);
+        }
+
+        [Test]
+        // [Ignore("This test is disabled")]
+        public static void TestThree()
+        {
+            Assert.AreEqual(Program.Factorial(7), 5040);
+        }
+    }
+}
---
+++ teamcity1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teamcity1
+{
+    public class Program
+    {
+        public static int Factorial(int n)
+        {
+            if (n == 1) return 1;
+
+            return n * Factorial(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+        }
+    }
+}
